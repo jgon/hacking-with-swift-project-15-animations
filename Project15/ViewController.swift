711aef196ed1e0e3ee6f3f774e807b7180adb01e
JackG <jacques.gonzalez@me.com>
2016-02-17 17:53:01 +0100

    Added damping and initial velocity to spring.

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -30,12 +30,35 @@ class ViewController: UIViewController {
     @IBAction func tapped(sender: UIButton) {
         tapButton.hidden = true
         
-        UIView.animateWithDuration(1, delay: 0, options: [],
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
             animations: { [unowned self] () -> Void in
                 switch self.currentAnimation {
                 case 0:
-                    break
+                    self.imageView.transform = CGAffineTransformMakeScale(2, 2)
                     
+                case 1:
+                    self.imageView.transform = CGAffineTransformIdentity
+                    
+                case 2:
+                    self.imageView.transform = CGAffineTransformMakeTranslation(-256, -256)
+
+                case 3:
+                    self.imageView.transform = CGAffineTransformIdentity
+                    
+                case 4:
+                    self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+                    
+                case 5:
+                    self.imageView.transform = CGAffineTransformIdentity
+                
+                case 6:
+                    self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = UIColor.greenColor()
+                    
+                case 7:
+                    self.imageView.alpha = 1
+                    self.imageView.backgroundColor = UIColor.clearColor()
+                
                 default:
                     break
                 }
